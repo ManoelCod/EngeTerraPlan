82aa59210d03b7389020ef1e9418e06b30929bd9
Manoel Neto <manoelcontatoneto@gmail.com>
2025-03-19 22:00:36 -0300

    feat: Modulo Densidade InSitu- Alternacia de formulario

--- EngeTerraPlan/ViewModels/DensidadeInSituViewModel.cs
+++ EngeTerraPlan/ViewModels/DensidadeInSituViewModel.cs
@@ -43,20 +43,49 @@ namespace EngeTerraPlan.ViewModels
             get => _espessuraCamada;
             set => SetProperty(ref _espessuraCamada, value);
         }
-
         public string PosicaoSelecionada
         {
             get => _posicaoSelecionada;
             set => SetProperty(ref _posicaoSelecionada, value);
         }
 
-        // Comando para o botão "Avançar"
-        public ICommand AvancarCommand { get; }
+        // comandos para alternar formulario
+
+        private bool _mostrarFormulario1 = true;
+        public bool MostrarFormulario1
+        {
+            get => _mostrarFormulario1;
+            set => SetProperty(ref _mostrarFormulario1, value);
+        }
+
+        private bool _mostrarFormulario2 = false;
+        public bool MostrarFormulario2
+        {
+            get => _mostrarFormulario2;
+            set => SetProperty(ref _mostrarFormulario2, value);
+        }
+
+        // Comando para o botão "Avançar e voltar"
+        public Command AvancarCommand { get; }
+        public Command VoltarCommand { get; }
 
         // Construtor
         public DensidadeInSituViewModel()
         {
-            AvancarCommand = new Command(ExecutarAvancar);
+            AvancarCommand = new Command(ExibirFormulario2);
+            VoltarCommand = new Command(ExibirFormulario1);
+        }
+
+        private void ExibirFormulario2()
+        {
+            MostrarFormulario1 = false;
+            MostrarFormulario2 = true;
+        }
+
+        private void ExibirFormulario1()
+        {
+            MostrarFormulario1 = true;
+            MostrarFormulario2 = false;
         }
 
         // Função executada ao pressionar o botão "Avançar"
