c972a5c21109c59004a257823fa5a25b3b109c2a
Manoel Neto <manoelcontatoneto@gmail.com>
2025-03-30 15:38:46 -0300

    fix: Modulo desidade in situ - salvar dados ajustes

---
+++ EngeTerraPlan/Ultils/RadioButtonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace EngeTerraPlan.Ultils
+{
+    public class RadioButtonConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value?.ToString() == parameter?.ToString();
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return (bool)value ? parameter?.ToString() : null;
+        }
+    }
+}
