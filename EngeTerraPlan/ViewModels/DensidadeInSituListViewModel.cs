97a71f9080837c84aa0ecee98aac864d860b5692
Manoel Neto <manoelcontatoneto@gmail.com>
2025-04-05 17:40:40 -0300

    feat: Modulo Densidade In-Situ: desenvolvimento  fluxo list cirar e nevagação

---
+++ EngeTerraPlan/ViewModels/DensidadeInSituListViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using EngeTerraPlan.Data;
+using EngeTerraPlan.Models;
+using EngeTerraPlan.Views;
+
+namespace EngeTerraPlan.ViewModels
+{
+    public class DensidadeInSituListViewModel : BaseViewModel
+    {
+        public ObservableCollection<DensidadeInSituModel> DensidadeItems { get; set; }
+        public ICommand LoadItemsCommand { get; }
+        public ICommand CreateDensidadeCommand { get; }
+
+        public DensidadeInSituListViewModel()
+        {
+            DensidadeItems = new ObservableCollection<DensidadeInSituModel>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            CreateDensidadeCommand = new Command(async () => await ExecuteCreateDensidadeCommand());
+        }
+
+        private async Task ExecuteLoadItemsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                DensidadeItems.Clear();
+                var items = await Task.Run(() => DatabaseService.Database.Table<DensidadeInSituModel>().ToList());
+                foreach (var item in items)
+                {
+                    DensidadeItems.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Erro ao carregar os itens: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task ExecuteCreateDensidadeCommand()
+        {
+            await Shell.Current.GoToAsync("///DensidadeInSituView");
+        }
+    }
+}
--- EngeTerraPlan/Views/DensidadeInSituListView.xaml.cs
+++ EngeTerraPlan/Views/DensidadeInSituListView.xaml.cs
@@ -1,44 +1,19 @@
 using System;
 using System.Collections.ObjectModel;
+using EngeTerraPlan.ViewModels;
 using Microsoft.Maui.Controls;
 
 namespace EngeTerraPlan.Views
 {
     public partial class DensidadeInSituListView : ContentPage
     {
-        public ObservableCollection<DensidadeItem> DensidadeItems { get; set; }
+        DensidadeInSituListViewModel viewModel;
 
         public DensidadeInSituListView()
         {
             InitializeComponent();
-            BindingContext = this; // Define o contexto de dados
-            LoadDensidadeItems(); // Carrega os itens
-        }
-
-        private void LoadDensidadeItems()
-        {
-            try
-            {
-                // Inicializa os dados
-                DensidadeItems = new ObservableCollection<DensidadeItem>
-                {
-                    new DensidadeItem { Codigo = "457889", Data = "25/03/2030", Cliente = "Martis LTDA", Obra = "Boulevard", EstacaLocal = "23", GrauCompactacao = "98%" },
-                    new DensidadeItem { Codigo = "457890", Data = "26/03/2030", Cliente = "Construtora X", Obra = "Projeto Y", EstacaLocal = "45", GrauCompactacao = "95%" }
-                
-                };
-
-                // Atualiza o BindingContext
-                OnPropertyChanged(nameof(DensidadeItems));
-
-                // Mensagem de verificação
-                System.Diagnostics.Debug.WriteLine($"Itens carregados: {DensidadeItems.Count}");
-            }
-            catch (Exception ex)
-            {
-                // Tratamento de erro
-                System.Diagnostics.Debug.WriteLine($"Erro ao carregar os itens: {ex.Message}");
-                DisplayAlert("Erro", "Não foi possível carregar os itens.", "OK");
-            }
+            BindingContext = viewModel = new DensidadeInSituListViewModel();
+            viewModel.LoadItemsCommand.Execute(null);
         }
     }
 
